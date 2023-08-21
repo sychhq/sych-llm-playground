c29e73326f159b052bd2790dbb61598628862cde
Ryan Haris <ryan.haris@sych.io>
2023-08-21 02:11:13 -0400

    Add functionality to create a Public HTTP endpoint.
    Issue: https://github.com/sychhq/sych-llm-playground/issues/30

--- src/sych_llm_playground/providers/aws/cleanup.py
+++ src/sych_llm_playground/providers/aws/cleanup.py
@@ -1,7 +1,7 @@
 """Module for cleaning up deployed resources on AWS.
 
-This module provides functions to delete deployed resources such as models
-and endpoints from the cloud. It includes functions to
+This module provides functions to delete deployed resources such as models,
+endpoints, and API Gateways from the cloud. It includes functions to
 select resources and initiate their cleanup.
 
 Functions:
@@ -10,9 +10,6 @@ Functions:
     cleanup: Main function for cleaning up resources.
 """
 
-from typing import Any
-from typing import cast
-
 import boto3
 import click
 import inquirer
@@ -20,59 +17,37 @@ import inquirer
 from ...utils.loader import start_loader
 from ...utils.loader import stop_loader
 from .utils.credentials import load_credentials
-from .utils.resources import get_resources
-
-
-def choose_resource(resource_type: str, sagemaker_client: Any) -> str:
-    """Choose a specific resource for cleanup from AWS.
-
-    This function lists the available resources of the specified type and prompts
-    the user to select one for deletion.
-
-    Args:
-        resource_type (str): The type of resource to delete (e.g., "Model", "Endpoint").
-        sagemaker_client (Any): The client to use to interact with AWS SageMaker.
-
-    Returns:
-        str: The name of the selected resource.
-    """
-    resources = get_resources(resource_type, sagemaker_client)
-    if len(resources) == 0:
-        exit(0)
-
-    questions = [
-        inquirer.List(
-            resource_type.lower(),
-            message=f"Select a {resource_type.lower()} to cleanup:",
-            choices=resources,
-        ),
-    ]
-
-    answers = inquirer.prompt(questions)
-    return cast(str, answers[resource_type.lower()])
+from .utils.resources import choose_resource
 
 
 def cleanup_resource(resource_type: str) -> None:
     """Delete a selected resource from AWS.
 
-    This function deletes a selected resource of the given type, either a model
-    or an endpoint. It manages the deletion process, including error handling
-    and success messaging.
+    This function deletes a selected resource of the given type, either a model,
+    an endpoint, or an API Gateway. It manages the deletion process,
+    including error handling and success messaging.
 
     Args:
-        resource_type (str): The type of resource to delete (e.g., "Model", "Endpoint").
+        resource_type (str): The type of resource to delete
+            (e.g., "Model", "Endpoint", "API Gateway").
     """
-    sagemaker_client = boto3.client("sagemaker")
-    selected_resource = choose_resource(resource_type, sagemaker_client)
+    client = (
+        boto3.client("sagemaker")
+        if resource_type != "API Gateway"
+        else boto3.client("apigateway")
+    )
+    selected_resource = choose_resource(resource_type, client, "cleanup")
 
     try:
         loader_thread = start_loader(
             message=f"Deleting selected {resource_type.lower()}...", color="green"
         )
         if resource_type == "Model":
-            sagemaker_client.delete_model(ModelName=selected_resource)
+            client.delete_model(ModelName=selected_resource["name"])
         elif resource_type == "Endpoint":
-            sagemaker_client.delete_endpoint(EndpointName=selected_resource)
+            client.delete_endpoint(EndpointName=selected_resource["name"])
+        elif resource_type == "API Gateway":
+            client.delete_rest_api(restApiId=selected_resource["id"])
         stop_loader(loader_thread)
 
     except Exception as e:
@@ -82,16 +57,14 @@ def cleanup_resource(resource_type: str) -> None:
         )
         exit(1)
 
-    click.secho(
-        f"{resource_type} {selected_resource} cleaned up successfully.", fg="green"
-    )
+    click.secho(f"{resource_type} cleaned up successfully. \n", fg="green")
 
 
 def cleanup() -> None:
     """Main function for cleaning up AWS resources.
 
-    This function is the main function to remove deployed models
-    and endpoints from AWS. It loads credentials,
+    This function is the main function to remove deployed models,
+    endpoints, and API Gateways from AWS. It loads credentials,
     asks the user what they'd like to clean up,
     and then calls the appropriate cleanup function.
     """
@@ -104,6 +77,7 @@ def cleanup() -> None:
             choices=[
                 "Model",
                 "Endpoint",
+                "API Gateway",
             ],
         ),
     ]
@@ -111,7 +85,4 @@ def cleanup() -> None:
     answers = inquirer.prompt(questions)
     selected_option = answers["cleanup_options"]
 
-    if selected_option == "Model":
-        cleanup_resource("Model")
-    elif selected_option == "Endpoint":
-        cleanup_resource("Endpoint")
+    cleanup_resource(selected_option)
